be766eb2a2e5818a6d0f9d64da536b1011178c49
abdulkarim-hamidi <abdulkarim.hamidi@yahoo.com>
2023-02-25 15:24:26 +0000

    lab completed

--- src/main/java/ArraysAreEqual.java
+++ src/main/java/ArraysAreEqual.java
@@ -1,13 +1,21 @@
+// import java.lang.reflect.Array;
+
 //see test cases.
 public class ArraysAreEqual {
     /**
-     * Check if two arrays are equal to each other. Return true if all of the values in a are
-     * identical to the ones in b, and false otherwise. So, {1,2,3,4} is equal only to {1,2,3,4}.
+     * Check if two arrays are equal to each other. Return true if all of the 
+     * values in a are
+     * identical to the ones in b, and false otherwise. So, {1,2,3,4} is equal 
+     * only to {1,2,3,4}.
      *
-     * Note: attempting to solve this problem with an expression like array1==array2 will not work,
-     * because even though the arrays may be equivalent, they can occupy different positions in memory,
-     * which will cause a==b to fail. When working with Objects (arrays are special objects,) == just
-     * compares locations in memory. Object1 == Object2 is only true when they are literally the same
+     * Note: attempting to solve this problem with an expression like 
+     * array1==array2 will not work,
+     * because even though the arrays may be equivalent, they can occupy 
+     * different positions in memory,
+     * which will cause a==b to fail. When working with Objects (arrays are 
+     * special objects,) == just
+     * compares locations in memory. Object1 == Object2 is only true when they 
+     * are literally the same
      * object in memory.
      *
      * @param a the first array.
@@ -15,6 +23,19 @@ public class ArraysAreEqual {
      * @return true if the values of a are equal to the values of b.
      */
     public boolean equal(int[] a, int[] b){
+        int length = a.length;
+        if (b.length != a.length)
         return false;
+        for (int i = 0; i < length; i++)
+        if(a[i] != b[i])
+        return false;
+        return true;
     }
+            
 }
+            
+        
+       
+        
+        
+    
